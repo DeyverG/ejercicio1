36ab14b74b020a9d677852ad8f43a17064f07f1f
DeyverG <34787440+DeyverG@users.noreply.github.com>
2020-11-06 16:08:29 -0500

    ultimos cambios

--- src/components/Tarea.js
+++ src/components/Tarea.js
@@ -2,17 +2,19 @@ import '../style/tarea.css'
 
 const Tarea = ({ titulo, descripcion, id, handleClick }) => {
 
-    return (
-        <>
-        <div className="contenedor-tarea">
-            <h2 className="negrita">{titulo}</h2>
-            <h5 className="negrita">{descripcion}</h5>
-        </div>
-        <div className="equis" id={id} onClick={handleClick}>
-            x
-        </div>
-        </>
-    );
+   return (
+      <>
+         <div className="contenedorTarea">
+            <div className="equis" id={id} onClick={handleClick} title="Eliminar">
+               <i className="fas fa-times" id={id} onClick={handleClick}></i>
+            </div>
+            <div className="contenedor-tarea">
+               <h2 className="negrita">{titulo}</h2>
+               <h5 className="negrita">{descripcion}</h5>
+            </div>
+         </div>
+      </>
+   );
 }
 
 export default Tarea;
